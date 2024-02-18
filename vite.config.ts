d6f5d25bf706eb89d6826e19e77282312e436905
Mateusz Siara <matek1143@gmail.com>
2024-02-18 10:36:02 +0100

    Add trailing slash

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/natru-ally',
+  base: '/natru-ally/',
   plugins: [react()],
   test: {
     globals: true,
